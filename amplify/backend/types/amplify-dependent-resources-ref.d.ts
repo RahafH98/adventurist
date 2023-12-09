dea5d48a28fc54484800e1c419586b55c6b86886
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-12-09 13:54:52 +0300

    Configuring Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "adventurist": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
